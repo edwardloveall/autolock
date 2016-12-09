856451ff1058c599c20283f9bdd195fee980c8c8
Edward Loveall <edward@edwardloveall.com>
2016-12-09 15:57:24 -0500

    Switch to changing the prefs with user defaults
    Turns out, you can use the user defaults system to change the screen lock

--- autolock.sh
+++ autolock.sh
@@ -2,9 +2,9 @@ HOME_SSID='Death Star'
 CURRENT_SSID=`/System/Library/PrivateFrameworks/Apple80211.framework/Versions/Current/Resources/airport -I | awk 'BEGIN { FS = ": " }; /[^B]SSID/ { print $2 }'`
 
 if [ "$CURRENT_SSID" = "$HOME_SSID" ]; then
-  PASS_ON_WAKE='false'
+  PASS_ON_WAKE='0'
 else
-  PASS_ON_WAKE='true'
+  PASS_ON_WAKE='1'
 fi
 
-osascript -e 'tell application "System Events"' -e 'tell security preferences' -e "set require password to wake to $PASS_ON_WAKE" -e 'end tell' -e 'end tell'
+defaults write com.apple.screensaver askForPassword -int $PASS_ON_WAKE
